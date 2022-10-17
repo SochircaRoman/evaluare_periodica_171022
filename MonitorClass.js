35cf19d6460369b599156df15719e136f17d6fde
User01 <romansochirca30@gmail.com>
2022-10-17 10:43:18 +0300

    Add getters and setters

--- MonitorClass.js
+++ MonitorClass.js
@@ -8,4 +8,30 @@ class Monitor {
     this.resolution = resolution
     this.#dislay = dislay
   }
+
+  // Getters
+  get brand() {
+    return this.brand
+  }
+
+  get resolution() {
+    return this.resolution
+  }
+
+  get dislay() {
+    return this.#dislay
+  }
+
+  // Setters
+  set changeBrand(newBrand) {
+    this.brand = newBrand
+  }
+
+  set changeResolution(newResolution) {
+    this.resolution = newResolution
+  }
+
+  set changeDislay(newDislay) {
+    this.#dislay = newDislay
+  }
 }
